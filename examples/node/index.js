1f695ca512de6ab7be2d13094a725b9ab0dcafcf
fredsonchaves07 <fredson.ribeiro@objective.com.br>
2022-12-20 19:48:38 -0300

    feat: mais exemplos de utilizacao de docker-compose

--- examples/node/index.js
+++ examples/node/index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+const sql = `insert into people(name) values('Fred')`
+connection.query(sql)
+connection.end()
 
 app.get("/", (req, res) => {
     res.send('<h1>Hello Full Cycle</h1>')
