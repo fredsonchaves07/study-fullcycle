2bb328f3ece1655305c71201aa4546167517e052
fredsonchaves07 <fredson.ribeiro@objective.com.br>
2023-04-12 21:51:59 -0300

    feat: organizando projeto grpc com java

---
+++ 2 - gRPC/java/category-grpc/src/main/java/com/fredsonchaves07/CategoryGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.fredsonchaves07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryGrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CategoryGrpcApplication.class, args);
+    }
+
+}
